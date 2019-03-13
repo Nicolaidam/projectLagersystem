5e31f65e885e79b7056b608dc2d2425427dacf2a
Nicolai Dam <nicolaidam96@gmail.com>
2019-03-13 14:53:44 +0100

    tryk på knap

--- LagerSystem/LagerSystem/Window1.xaml.cs
+++ LagerSystem/LagerSystem/Window1.xaml.cs
@@ -10,28 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Navigation;
 
 namespace LagerSystem
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-          
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Window1 win2 = new Window1();
-            win2.Show();
-            this.Close();
         }
     }
 }
