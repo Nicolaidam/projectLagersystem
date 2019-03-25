ba798f5f13340fe7d535585ce56dd0684c474f59
Nicolai Dam <nicolaidam96@gmail.com>
2019-03-25 13:54:26 +0100

    login db

--- LagerSystem/LagerSystem/Login.xaml.cs
+++ LagerSystem/LagerSystem/Login.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,55 @@ namespace LagerSystem
             InitializeComponent();
         }
 
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"|DataDirectory|\\Database.mdf\";Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        private String getBrugernavn(String password)
+        {
+            con.Open();
+            String syntax = "SELECT brugernavn FROM Login WHERE password = '" + password + "'";
+            cmd = new SqlCommand(syntax, con);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            String temp = "0";
+
+            try
+            {
+                temp = dr[0].ToString();
+            }
+            catch (System.InvalidOperationException)
+            {
+
+            }
+
+            con.Close();
+            return temp;
+        }
+
+        private String getPassword(String brugernavn)
+        {
+
+            con.Open();
+            String syntax = "SELECT password FROM Login WHERE brugernavn = '" + brugernavn + "'";
+            cmd = new SqlCommand(syntax, con);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            String temp = "1";
+
+            try
+            {
+                temp = dr[0].ToString();
+            }
+            catch (System.InvalidOperationException)
+            {
+
+            }
+            con.Close();
+
+            return temp;
+        }
+
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
@@ -37,8 +87,8 @@ namespace LagerSystem
         private void Button_Click(object sender, RoutedEventArgs e)
 
         {
-            
-                        
+
+
 
         }
 
@@ -46,9 +96,32 @@ namespace LagerSystem
         {
             //TODO lav verifisering af brugernavn og password
 
-            Main h = new Main();
-            h.Show();
-            this.Hide();
+            String inputBrugernavn, inputPassword, dbBrugernavn, dbPassword;
+
+            inputBrugernavn = textboxBrugernavn.Text;
+            inputPassword = textboxPassword.Password;
+
+
+            dbBrugernavn = getBrugernavn(inputPassword);
+            dbPassword = getPassword(inputBrugernavn);
+
+
+            Console.WriteLine(inputBrugernavn);
+            Console.WriteLine(inputPassword);
+            Console.WriteLine(dbBrugernavn);
+            Console.WriteLine(dbPassword);
+
+            if (inputBrugernavn.Equals(dbBrugernavn) && inputPassword.Equals(dbPassword))
+            {
+                Main h = new Main();
+                h.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Forkert login");
+            }
+
 
         }
     }
