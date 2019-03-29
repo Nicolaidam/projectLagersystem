d25e2ac9d13b6ad6f0cb914f6aade2674745203d
Jacob <jacob-johan@hotmail.com>
2019-03-29 21:11:47 +0100

    Startet på så man kan oprette nye Items. Det kan gøres nogenlunde, men der mangler stadig en del

--- LagerSystem/LagerSystem/Main.xaml.cs
+++ LagerSystem/LagerSystem/Main.xaml.cs
@@ -26,19 +26,37 @@ namespace LagerSystem
         public Main()
         {
             InitializeComponent();
+            combo.Items.
+                Add("Mobil");
+            combo.Items.
+                Add("PC");
+            combo.Items.
+                Add("PC-dele");
+            combo.Items.
+                Add("Andet");
             Logik logik = Logik.Instance;
-            logik.addItem(new Item { Afdeling = "670", Ejer = "Jacob", Id = "qwe", Lokation = "h", Maerke = "kk", Model = "din mor", Note = "hhhhhh", Pris = 123 });
+            logik.addItem(new Item { Ejer = "Jacob", Id = "qwe", Lokation = "h", Maerke = "kk", Model = "din mor", Note = "hhhhhh", Pris = 123 });
             logik.addItem(new Mobil { Afdeling = "470", Ejer = "Anders", Id = "qwe", Lokation = "h", Maerke = "kk", Model = "din mor", Note = "hhhhhh", Pris = 123 });
             logik.addItem(new Item { Afdeling = "30", Ejer = "Brian", Id = "qwe", Lokation = "h", Maerke = "kk", Model = "din mor", Note = "hhhhhh", Pris = 123 });
             myDataGrid.ItemsSource = logik.AlleItems;
+            
 
 
 
-           
         }
 
+        
+
+        private void combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+                     
+        }
+
+        //opret knap
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            //lige nu kan der oprettes ny, men ikke med den der combobox, alt oprettes som Item
+            MessageBox.Show(combo.Text);
+            Logik.Instance.addItem(new Item {Ejer = ejer.Text, Afdeling = afd.Text, Maerke = maerke.Text, Model = model.Text });
         }
     } }
