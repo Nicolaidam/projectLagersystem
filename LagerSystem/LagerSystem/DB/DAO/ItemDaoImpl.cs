d7b0b18b7e13f00cbb529e7331f488f9244915b1
Nicolai Dam <nicolaidam96@gmail.com>
2019-04-03 18:40:19 +0200

    stan

---
+++ LagerSystem/LagerSystem/DB/DAO/ItemDaoImpl.cs
@@ -0,0 +1,49 @@
+﻿using LagerSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerSystem.DAO
+{
+    
+    class ItemDaoImpl : ItemDao
+    {
+        List<Item> items;
+        public void DeleteItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Item> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Item> GetAllMobil()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Item> GetAllPC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Item> GetAllPCDele()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
